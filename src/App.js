da0c2344480cc7319d8856cc10ac51ccc454f7cf
Alex Brashear <alexbrashear@gmail.com>
2019-05-01 11:27:55 -0400

    Basic app setup done

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import firebase from 'firebase';
+
+class App extends Component {
+    
+    componentWillMount() {
+          // Initialize Firebase
+        const config = {
+            apiKey: 'AIzaSyDkYG1EQ81juAodMXl82x0GE9T_viESHwo',
+            authDomain: 'manager-1127c.firebaseapp.com',
+            databaseURL: 'https://manager-1127c.firebaseio.com',
+            projectId: 'manager-1127c',
+            storageBucket: 'manager-1127c.appspot.com',
+            messagingSenderId: '960465752980'
+        };
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        //createStore creates a Redux store that holds the complete state tree of our app. 
+        //The Provider provides our store so it is available throughout out applications.
+        return (
+            <Provider store={createStore(reducers)}>
+                <View>
+                    <Text>
+                        Hello!
+                    </Text>
+                </View>
+            </Provider>
+        );
+    }
+}
+
+export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+//We will wire all of our reducers together here
+import { combineReducers } from 'redux';
+
+export default combineReducers({
+    banana: () => []
+});
