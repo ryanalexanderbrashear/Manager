ec998480cb908da4568f3f86d7d513432201e495
Alex Brashear <alexbrashear@gmail.com>
2019-05-23 08:38:27 -0400

    Added in the EmployeeList component

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Alex</Text>
+                <Text>Brashear</Text>
+            </View>
+        );
+    }
+}
+
+export default EmployeeList;
