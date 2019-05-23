17065573f2ed3858780032bbef9a6435df0deb7f
Alex Brashear <alexbrashear@gmail.com>
2019-05-23 08:55:59 -0400

    Added in routing buckets to separate auth and main flows. Added right button to employee list screen which will be used to navigate to the create employee screen

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from "./types";
+import { Actions } from 'react-native-router-flux';
 
 //Action Creators
 
@@ -36,6 +37,8 @@ const loginUserSuccess = (dispatch, user) => {
         type: LOGIN_USER_SUCCESS,
         payload: user
     });
+    //Using react-native-router-flux, each scene we create has a function created using the key property. We can navigate to that screen using that function
+    Actions.main();
 }
 
 const loginUserFail = (dispatch) => {
