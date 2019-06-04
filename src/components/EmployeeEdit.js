e6e199958586b830881033147953737523a80c6e
Alex Brashear <alexbrashear@gmail.com>
2019-06-04 19:50:50 -0400

    Application is now complete

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,12 +1,14 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { Card, CardSection, Button } from './common';
+import { Card, CardSection, Button, Confirm } from './common';
 import { connect } from 'react-redux';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import EmployeeForm from './EmployeeForm';
 import Communications from 'react-native-communications';
 
 class EmployeeEdit extends Component {
+
+    state = { showModal: false };
 
     componentWillMount() {
         _.each(this.props.employee, (value, prop) => {
@@ -26,6 +28,16 @@ class EmployeeEdit extends Component {
         Communications.text(phone, `Your upcoming shift is on ${shift}. :)`)
     }
 
+    onAccept() {
+        const { uid } = this.props.employee
+
+        this.props.employeeDelete({ uid });
+    }
+
+    onDecline() {
+        this.setState({ showModal: false });
+    }
+
     render() {
         return (
             <Card>
@@ -43,6 +55,21 @@ class EmployeeEdit extends Component {
                         onPress={this.onTextPress.bind(this)}
                     />
                 </CardSection>
+
+                <CardSection>
+                    <Button
+                        title='Fire Employee'
+                        onPress={() => this.setState({ showModal: !this.state.showModal })}
+                    />
+                </CardSection>
+
+                <Confirm 
+                    visible={this.state.showModal}
+                    onAccept={this.onAccept.bind(this)}
+                    onDecline={this.onDecline.bind(this)}
+                >
+                    Are you sure you want to delete this?
+                </Confirm>
             </Card>
         );
     }
@@ -54,4 +81,4 @@ const mapStateToProps = (state) => {
     return { name, phone, shift };
 }
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
---
+++ src/components/common/Confirm.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Text, View, Modal } from 'react-native';
+import { CardSection } from './CardSection';
+import { Button } from './Button';
+
+const Confirm = ({ children, visible, onAccept, onDecline }) => {
+
+    const { cardSectionStyle, textStyle, containerStyle } = styles;
+
+    return (
+        <Modal
+            visible={visible}
+            animationType='slide'
+            onRequestClose={() => {}} //On Android you have to pass in this prop no matter whether you're doing something or not
+            transparent
+        >
+            <View style={containerStyle}>
+                <CardSection style={cardSectionStyle}>
+                    <Text style={textStyle}>{children}</Text>
+                </CardSection>
+
+                <CardSection>
+                    <Button onPress={onAccept} title='Yes' />
+                    <Button onPress={onDecline} title='No' />
+                </CardSection>
+            </View>
+        </Modal>
+    );
+};
+
+const styles = {
+    cardSectionStyle: {
+        justifyContent: 'center'
+    },
+    textStyle: {
+        flex: 1,
+        fontSize: 18,
+        textAlign: 'center',
+        lineHeight: 40
+    },
+    containerStyle: {
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+        position: 'relative',
+        flex: 1,
+        justifyContent: 'center'
+    }
+};
+
+export { Confirm };
